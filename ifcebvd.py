0de5b98544c723ac08aa7b525f77b25ad5a8f309
Bruno Macabeus <bruno.macabeus@gmail.com>
2016-01-01 05:10:17 -0300

    Adicionado timeout

--- ifcebvd.py
+++ ifcebvd.py
@@ -5,6 +5,7 @@
 # /_/_/  BIBLIOTECA VIRTUAL DOWNLOADER
 
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 import urllib
 import hashlib
 import sys
@@ -25,10 +26,23 @@ _hash = lambda _mat: 'login=%s&token=%s' % (_mat, hashlib.md5(('%sQJEkJM2iLJiAj6
 
 def _dump(matricula, id_livro):
     phantom = webdriver.PhantomJS()
+    phantom.set_page_load_timeout(10)
+
+    # Login
     print('gerando cookie de login para matricura %s...' % matricula)
-    phantom.get('http://ifce.bv3.digitalpages.com.br/user_session/authentication_gateway?%s' % _hash(matricula))
+    try:
+        phantom.get('http://ifce.bv3.digitalpages.com.br/user_session/authentication_gateway?%s' % _hash(matricula))
+    except TimeoutException:
+        print('Timeout durante o login... Refazendo-o...')
+        return _dump(matricula, id_livro)
+
+    # Obter informações do livro
     print('obtendo informacoes para o livro %s...' % id_livro)
-    phantom.get('http://ifce.bv3.digitalpages.com.br/users/publications/%s' % id_livro)
+    try:
+        phantom.get('http://ifce.bv3.digitalpages.com.br/users/publications/%s' % id_livro)
+    except TimeoutException as e:
+        print('Timeout durante a coleta das informações do livro... Recomeçando tudo...')
+        return _dump(matricula, id_livro)
     p_1 = 0
     while p_1 == 0:
         try:
@@ -36,6 +50,8 @@ def _dump(matricula, id_livro):
         except:
             p_1 = 0
     num_pag = int(phantom.execute_script('return RDP.options.pageSetLength')) - 2
+
+    # Download
     print('preparando para baixar livro id=%s com %d paginas...' % (id_livro, num_pag))
     _baixa(phantom.execute_script("return $('.backgroundImg')[0].src"), '%s-00000.jpg' % id_livro)
     print('baixando livro...')
@@ -43,8 +59,8 @@ def _dump(matricula, id_livro):
     _v_p1, _v_p2 = '', ''
     for i in range(1, num_pag, 2):
         # loop para esperar a pagina carregar
-        p_1 , p_2 = 0, 0
-        while ((p_1==0) or (p_2==0)):
+        p_1, p_2 = 0, 0
+        while (p_1 == 0) or (p_2 == 0):
             try:
                 p_1 = phantom.execute_script("if ($('.backgroundImg')[0]) { return 1 } else { return 0 }")
                 p_2 = phantom.execute_script("if ($('.backgroundImg')[1]) { return 1 } else { return 0 }")
@@ -67,16 +83,18 @@ def _dump(matricula, id_livro):
         _v_p1, _v_p2 = _p1, _p2
         phantom.execute_script('navigate.next_page()')
         print('baixadas %d/%d paginas...' % (i,num_pag))
+
+    # Finalização
     print('fim do dump')
     phantom.quit()
 
 
 def _gerapdf(_livro):
-    # usando a ferramenta convert do ImageMagick
     print('convertendo para PDF...')
     import os
     so_corrente = platform.system()
     if so_corrente == 'Linux':
+        # usando a ferramenta convert do ImageMagick
         os.system('convert *.jpg %s.pdf' % _livro)
         print('limpando os jpgs residuais...')
         os.system('rm *.jpg')
@@ -84,23 +102,23 @@ def _gerapdf(_livro):
         from fpdf import FPDF
         from PIL import Image
         import glob
-        listPages = glob.glob('*.jpg')
-        cover = Image.open(str(listPages[0]))
+        list_pages = glob.glob('*.jpg')
+        cover = Image.open(str(list_pages[0]))
         width, height = cover.size
-        pdf = FPDF(unit = 'pt', format = [width, height])
-        for page in listPages:
+        pdf = FPDF(unit='pt', format=[width, height])
+        for page in list_pages:
             pdf.add_page()
             pdf.image(str(page), 0, 0)
         pdf.output('%s.pdf' % _livro, 'F')
         del cover
-        for page in listPages:
+        for page in list_pages:
             os.remove(page)
     else:
         print('nao e possivel gerar pdf nesse sistema')
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 1:
+    if len(sys.argv) == 1:
         print("Falta fornecer parâmetros!\n"
               "Sintaxe: ifcevd.py <número da matrícula> <endereco url do livro no bvu> [endereco url de outro livro] ..")
         exit()
