3a9f6f398f714db626081c4b722c8503f8cd272d
Unknown <fausto.level@gmail.com>
2018-07-08 09:25:47 -0300

    mudanças feitas para melhorar a qualidade e adcionar um seletor dela

--- download.py
+++ download.py
@@ -13,6 +13,18 @@ import sys
 import re
 import os
 
+def whileZoom(phantom):
+    zoom = 0
+    while zoom <= 2:
+        phantom.execute_script("document.getElementById('sunflower_zoom_bar').setAttribute('style', 'visibility: visible;')")
+        phantom.find_element_by_css_selector('.zoom_btn.zoom_in').click()
+
+        # phantom.execute_script("$('.zoom_btn.zoom_in').click()")
+        zoom = phantom.execute_script('return parseInt(RDP.zoom.current)')
+        print('atual zoom in = %s' % zoom)
+        if zoom >= 3:
+            phantom.get_screenshot_as_file('ss/error-1.png')
+            break
 
 def page_download(_url, _nome):
     try:
@@ -28,7 +40,7 @@ def page_download(_url, _nome):
         print('%s baixado' % _nome)
 
 
-def dump(matricula, senha, id_book):
+def dump(matricula, senha, quality, id_book):
     phantom = webdriver.Chrome()
     phantom.set_window_size(1366, 900) # set browser size.
     phantom.set_page_load_timeout(10)
@@ -45,7 +57,7 @@ def dump(matricula, senha, id_book):
         phantom.save_screenshot('ss/user_%s_ufersa_afterlogin.png' % matricula)
     except TimeoutException:
         print('Timeout durante o login... Refazendo-o...')
-        return dump(matricula, senha, id_book)
+        return dump(matricula, senha, quality, id_book)
 
     # Obter informações do livro
     print('obtendo informacoes para o livro %s...' % id_book)
@@ -54,7 +66,7 @@ def dump(matricula, senha, id_book):
         phantom.get('https://ufersa.bv3.digitalpages.com.br/users/publications/%s' % id_book)
     except TimeoutException as e:
         print('Timeout durante a coleta das informações do livro... Recomeçando tudo...')
-        return dump(matricula, senha, id_book)
+        return dump(matricula, senha, quality, id_book)
     p_1 = 0
     count = 0
     location = phantom.execute_script('return document.location.href')
@@ -77,20 +89,10 @@ def dump(matricula, senha, id_book):
             # count = count + 1
             # print(count)
             if p_1 == 1:
-                zoom = 0
-                while True:
-                    phantom.execute_script("document.getElementById('sunflower_zoom_bar').setAttribute('style', 'visibility: visible;')")
-                    phantom.find_element_by_css_selector('.zoom_btn.zoom_in').click()
-
-                    # phantom.execute_script("$('.zoom_btn.zoom_in').click()")
-                    zoom = phantom.execute_script('return parseInt(RDP.zoom.current)')
-                    print('atual zoom in = %s' % zoom)
-                    if zoom == 3:
-                        phantom.get_screenshot_as_file('ss/error-1.png')
-                        break
+                whileZoom(phantom)
 
                 phantom.get_screenshot_as_file('ss/error-2.png')
-                page_download(phantom.execute_script('return (RDP.reader.currentPages[0].portrait.zoom[%s])' % zoom), 'book/%s-00000.jpg' % id_book)
+                page_download(phantom.execute_script('return (RDP.reader.currentPages[0].portrait.zoom[%s])' % quality), 'book/[%s]%s-00000.jpg' % (quality, id_book))
         except:
             p_1 = 0
     num_pag = int(phantom.execute_script('return RDP.options.pageSetLength')) - 2
@@ -98,8 +100,8 @@ def dump(matricula, senha, id_book):
     
     # Download
     print('preparando para baixar livro id=%s com %d paginas...' % (id_book, num_pag))
-    p0 = phantom.execute_script('return (RDP.reader.currentPages[0].portrait.zoom[%s])' % zoom)
-    page_download(p0, 'book/%s-00000.jpg' % id_book)
+    p0 = phantom.execute_script('return (RDP.reader.currentPages[0].portrait.zoom[%s])' % quality)
+    page_download(p0, 'book/[%s]%s-00000.jpg' % (quality, id_book))
     
     phantom.get_screenshot_as_file('ss/user_%s_ufersa_capa_do_livro.png' % matricula)
 
@@ -112,47 +114,27 @@ def dump(matricula, senha, id_book):
 
         while (p_1 == 0) or (p_2 == 0):
             try:
-                zoom = 0
-                while True:
-                    phantom.execute_script("document.getElementById('sunflower_zoom_bar').setAttribute('style', 'visibility: visible;')")
-                    phantom.find_element_by_css_selector('.zoom_btn.zoom_in').click()
+                whileZoom(phantom)
 
-                    # phantom.execute_script("$('.zoom_btn.zoom_in').click()")
-                    zoom = phantom.execute_script('return parseInt(RDP.zoom.current)')
-                    print('atual zoom in = %s' % zoom)
-                    if zoom == 3:
-                        phantom.get_screenshot_as_file('ss/error-1.png')
-                        break
-
-                p_1 = phantom.execute_script("if (RDP.reader.currentPages[0].portrait.zoom[%s]) { return 1 } else { return 0 }" % zoom)
-                p_2 = phantom.execute_script("if (RDP.reader.currentPages[0].portrait.zoom[%s]) { return 1 } else { return 0 }" % zoom)
+                p_1 = phantom.execute_script("if (RDP.reader.currentPages[0].portrait.zoom[%s]) { return 1 } else { return 0 }" % quality)
+                p_2 = phantom.execute_script("if (RDP.reader.currentPages[1].portrait.zoom[%s]) { return 1 } else { return 0 }" % quality)
             except:
                 p_1 , p_2 = 0, 0
         # carregou, pegar endereco
-        _p1 = phantom.execute_script('return (RDP.reader.currentPages[0].portrait.zoom[%s])' % zoom)
-        _p2 = phantom.execute_script('return (RDP.reader.currentPages[1].portrait.zoom[%s])' % zoom)
+        _p1 = phantom.execute_script('return (RDP.reader.currentPages[0].portrait.zoom[%s])' % quality)
+        _p2 = phantom.execute_script('return (RDP.reader.currentPages[1].portrait.zoom[%s])' % quality)
         # checa se ainda não carregou a nova pagina
         while (_p1 == _v_p1) or (_p2 == _v_p2):
             try:
-                zoom = 0
-                while True:
-                    phantom.execute_script("document.getElementById('sunflower_zoom_bar').setAttribute('style', 'visibility: visible;')")
-                    phantom.find_element_by_css_selector('.zoom_btn.zoom_in').click()
-
-                    # phantom.execute_script("$('.zoom_btn.zoom_in').click()")
-                    zoom = phantom.execute_script('return parseInt(RDP.zoom.current)')
-                    print('atual zoom in = %s' % zoom)
-                    if zoom == 3:
-                        phantom.get_screenshot_as_file('ss/error-1.png')
-                        break
+                whileZoom(phantom)
                         
-                _p1 = phantom.execute_script('return (RDP.reader.currentPages[0].portrait.zoom[%s])' % zoom)
-                _p2 = phantom.execute_script('return (RDP.reader.currentPages[1].portrait.zoom[%s])' % zoom)
+                _p1 = phantom.execute_script('return (RDP.reader.currentPages[0].portrait.zoom[%s])' % quality)
+                _p2 = phantom.execute_script('return (RDP.reader.currentPages[1].portrait.zoom[%s])' % quality)
             except:
                 pass
         # carregou nova, baixar...
-        page_download(_p1, 'book/%s-%05d.jpg' % (id_book, i))
-        page_download(_p2, 'book/%s-%05d.jpg' % (id_book, i+1))
+        page_download(_p1, 'book/[%s]%s-%05d.jpg' % (quality, id_book, i))
+        page_download(_p2, 'book/[%s]%s-%05d.jpg' % (quality, id_book, i+1))
         # ajusta os novos valores
         _v_p1, _v_p2 = _p1, _p2
         phantom.execute_script('navigate.next_page()')
@@ -163,7 +145,7 @@ def dump(matricula, senha, id_book):
     phantom.quit()
 
 
-def make_pdf(_livro):
+def make_pdf(_quality, _livro):
     print('convertendo para PDF...')
     from fpdf import FPDF
     from PIL import Image
@@ -176,7 +158,7 @@ def make_pdf(_livro):
         print(page)
         pdf.add_page()
         pdf.image(str(page), 0, 0)
-    pdf.output('books/%s.pdf' % _livro, 'F')
+    pdf.output('books/[%s]_%s.pdf' % (_quality, _livro), 'F')
     for page in list_pages:
         os.remove(page)
 
@@ -184,14 +166,18 @@ def make_pdf(_livro):
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("Falta fornecer parâmetros!\n"
-              "Sintaxe: download.py <funcao> <número da matrícula> <senha> <endereco url do livro no bvu> [endereco url de outro livro] ..")
+              "Sintaxe: download.py <funcao> <número da matrícula> <senha> <quality 0, 1 or 3> <endereco url do livro no bvu> [endereco url de outro livro] ..")
         exit()
 
-    _, matricula, senha, *book_list = sys.argv
+    _, matricula, senha, quality, *book_list = sys.argv
 
     book_list = map(lambda i: i if (i.isdigit()) else re.match(r'(?:.*publications\/(\d+)|(\d+))', i).group(1), book_list)
 
+    if quality == 2: # or quality >= 4:
+        print("qualidade incorreta, use 0, 1 ou 3")
+        exit()
+
     for livro_atual in book_list:
-        dump(matricula, senha, livro_atual)
-        make_pdf(livro_atual)
+        dump(matricula, senha, quality, livro_atual)
+        make_pdf(quality, livro_atual)
     print('operacao finalizada.')
